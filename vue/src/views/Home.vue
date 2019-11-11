7f1d95300b3b8e35a77b1e51e8d7f2fc5b5131d4
TeamDman <TeamDman9201@gmail.com>
2019-11-10 20:13:05 -0500

    Create generic bootstrap vue project

---
+++ vue/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-button>yeet</b-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    
+  }
+}
+</script>
